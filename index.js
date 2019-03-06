4e3b6e27b94b8f795a8a802f6afcb5582c483c0f
alpha <dentalon599@gmail.com>
2019-03-06 14:14:12 +0300

    Added an interaction (adding orders in real-time)

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 const express = require("express");
+const http = require("http");
 const passport = require("passport");
 const session = require("express-session");
 const mailer = require("express-mailer");
-// const RedisStore = require('connect-redis')(session)
 
 const app = express();
-// app.use(session({
-//     store: new RedisStore({
-//         url: config.redisStore.url
-//     }),
-//     secret: config.redisStore.secret,
-//     resave: false,
-//     saveUninitialized: false
-// }))
-// app.use(passport.initialize())
-// app.use(passport.session())
+
 const db = require("./db");
 const bodyParser = require("body-parser");
-
 // Creating the parser for data application/x-www-form-urlencoded
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+var server = app.listen(3000);
+var io = require("socket.io").listen(server);
+
+var repairers = new Set();
+
 app.get("/", function(req, res) {
-  // res.sendFile(__dirname + "/public/index.html");
   db.query("SELECT * FROM deviceviews", (err, rows) => {
     res.render("index", { views: rows });
   });
 });
-
 app.post("/createOrder", urlencodedParser, function(req, res) {
   db.query(
-    "INSERT INTO Orders (idOrder, clientName, email, idView, idRepairer, idStatus, clientNumber, orderStartDate, clientMessage) " +
-      "VALUES (LAST_INSERT_ID(), ?, ?, ?, NULL, 1, ?, CURDATE(), ?) ",
+    "INSERT INTO Orders (clientName, email, idView, idRepairer, idStatus, clientNumber, orderStartDate, clientMessage) " +
+      "VALUES (?, ?, ?, NULL, 1, ?, CURDATE(), ?) ",
     [
       req.body.clientName,
       req.body.clientEmail,
       req.body.device_view,
       req.body.clientNumber,
-      req.body.clientMessage,
+      req.body.clientMessage
     ],
     (err, rows) => {
-      if (err) {
-        console.log("Error " + err);
-        throw err;
-      }
       console.log("The order was added");
-      console.log(rows);
-      res.sendFile(__dirname + "/public/successSendOrder.html");
+      db.query(
+        "SELECT nameView FROM deviceviews WHERE idView = ?",
+        [req.body.device_view],
+        (err, views) => {
+          var idOrder = 0;
+          db.query("SELECT MAX(idOrder) as lid FROM Orders", (err, data) => {
+            repairers.forEach(repairer => {
+              repairer.emit("newOrder", {
+                idOrder: data[0].lid,
+                clientName: req.body.clientName,
+                clientEmail: req.body.clientEmail,
+                view: views[0].nameView,
+                clientNumber: req.body.clientNumber,
+                clientMessage: req.body.clientMessage
+              });
+            });
+            res.sendFile(__dirname + "/public/successSendOrder.html");
+          });
+        });
     }
   );
 });
@@ -62,38 +68,50 @@ app.get("/orders", function(req, res) {
     (err, orders) => {
       db.query(
         "SELECT * FROM service.Orders o INNER JOIN statusOrder sO ON o.idStatus = sO.idStatus " +
-        "INNER JOIN deviceviews dv ON o.idView = dv.idView WHERE o.idRepairer IS NULL ORDER BY o.orderStartDate DESC",
+          "INNER JOIN deviceviews dv ON o.idView = dv.idView WHERE o.idRepairer IS NULL ORDER BY o.orderStartDate DESC",
         (err, news) => {
-      db.query("SELECT * FROM statusOrder ORDER BY idStatus", (err, statuses) => {
-        res.render("orders", { 
-          orders: orders,
-          unoccupied: news,
-          statuses: statuses,
-          idRepairer: req.query.idRepairer,
-         });
-      });
-    });
-    });
+          db.query(
+            "SELECT * FROM statusOrder ORDER BY idStatus",
+            (err, statuses) => {
+              io.sockets.on("connection", socket => {
+                repairers.add(socket);
+                io.sockets.on("disconnect", () => {
+                  repairers.delete(socket);
+                });
+              });
+              res.render("orders", {
+                orders: orders,
+                unoccupied: news,
+                statuses: statuses,
+                idRepairer: req.query.idRepairer
+              });
+            }
+          );
+        }
+      );
+    }
+  );
 });
 
 app.post("/changeStatus", urlencodedParser, function(req, res) {
-  var change_request = "UPDATE Orders SET idStatus = ?, idRepairer = " + req.body.idRepairer + " WHERE idOrder = ?";
+  var change_request =
+    "UPDATE Orders SET idStatus = ?, idRepairer = " +
+    req.body.idRepairer +
+    " WHERE idOrder = ?";
   if (req.body.orderStatus == 1) {
-    change_request = "UPDATE Orders SET idStatus = ?, idRepairer = NULL WHERE idOrder = ?"
+    change_request =
+      "UPDATE Orders SET idStatus = ?, idRepairer = NULL WHERE idOrder = ?";
   }
   db.query(
     change_request,
-    [
-      req.body.orderStatus,
-      req.body.idOrder,
-    ],
+    [req.body.orderStatus, req.body.idOrder],
     (err, rows) => {
       if (err) {
         console.log("Error " + err);
         throw err;
       }
-      console.log('Status #' + req.body.idOrder + ' was successfully changed!');
-      res.json({answer: 'OK',})
+      console.log("Status #" + req.body.idOrder + " was successfully changed!");
+      res.json({ answer: "OK" });
     }
   );
 });
@@ -101,4 +119,3 @@ app.post("/changeStatus", urlencodedParser, function(req, res) {
 app.get(function(req, res) {
   res.sendFile(__dirname + "/public/404.html");
 });
-app.listen(3000);
